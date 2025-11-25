29ce177a6f83b195979a86ae02a451412c168194
Mr.Carnahan <36049685+Mr-Carnahan@users.noreply.github.com>
2025-11-25 14:28:05 -0500

    Create printpractice.py
    Added instructions + starter code

---
+++ printpractice.py
@@ -0,0 +1,10 @@
+# Name: [Student Name]
+# Assignment 1 - Print Statement Practice
+
+# INSTRUCTIONS: Use the print() function below to output a three-line address,
+# similar to the example shown in the README.md file. Remember, dont use your actual addres!
+
+# Start your code here:
+
+print("Hello World! Here's an example print statement!")
+# Change the first print statement to use your name as the output. Then add the additional lines for the address and city/state/zip on the lines below.
